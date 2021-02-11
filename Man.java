9acc8db59da8a7067e07672297d65c9114ee209f
petrdixon <74814808+petrdixon@users.noreply.github.com>
2021-02-11 11:22:58 +0300

    Delete Man.java

--- Man.java
+++
@@ -1,7 +0,0 @@
-package Lesson1bis;
-
-public class Man implements Action{
-    public String entity = "Man";
-    public int powerRun = 20; //сколько метров всего пробежит
-    public int powerJump = 10; //сколько метров всего сможет перепрыгнуть
-}
