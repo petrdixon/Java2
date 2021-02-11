ceb5ac7366cd7b3d2611bb00c6b80c87dc71865b
petrdixon <74814808+petrdixon@users.noreply.github.com>
2021-02-11 11:23:07 +0300

    Delete Robot.java

--- Robot.java
+++
@@ -1,7 +0,0 @@
-package Lesson1bis;
-
-public class Robot implements Action{
-    public String entity = "Robot";
-    public int powerRun = 40; //сколько метров всего пробежит
-    public int powerJump = 20; //сколько метров всего сможет перепрыгнуть
-}
